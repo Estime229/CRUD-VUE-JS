e98b71cfeec2b4593eac45f0d2c77c593c04bc63
estime229 <estimegliti108@gmail.com>
2023-02-02 09:35:54 +0100

    crud

---
+++ js/app.js
@@ -0,0 +1,130 @@
+const checkIfEmptyValue = (obj) => {
+    let isEmpty = false;
+    for (const prop in obj) {
+        if (obj[prop] == "") {
+            isEmpty = true;
+        }
+
+    }
+    return isEmpty
+}
+
+
+const showSuccessMessage = (message)=>{
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: message,
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+
+
+const showErrorMessage = (message)=>{
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops.....',
+        text: message,
+        
+    })
+}
+
+const App = {
+    data() {
+        return {
+            showHome: false,
+            showCreateForm: false,
+            showStudentsList: false,
+
+
+
+            newStudent: {
+                nom: "",
+                prenom: "",
+                dateNaissance: "",
+                niveauScolaire: "",
+            }
+
+        }
+    },
+
+
+    mounted() {
+        this.changeNavigationState("home")
+    },
+
+
+
+
+
+    methods: {
+        goToHome() {
+            this.changeNavigationState("home")
+        },
+
+        goToCreateForm() {
+            this.changeNavigationState("create")
+        },
+
+        goToStudentsList() {
+            this.changeNavigationState("list")
+        },
+
+
+        submitStudent() {
+            // console.log(this.newStudent)
+            if (!checkIfEmptyValue(this.newStudent)) {
+            if(!checkIfStudentExist(this.newStudent.nom, this.newStudent.prenom)){
+                   addStudent(this.newStudent)
+             this.newStudent= {
+                    nom: "",
+                    prenom: "",
+                    dateNaissance: "",
+                    niveauScolaire: "",
+                }
+                showSuccessMessage("étudiant ajouté avec succès")
+            }else{
+                showErrorMessage("Cet étudiant existe déjà")
+            }
+
+
+            } else {
+                showErrorMessage("Veuillez remplir tout les champs")
+            }
+        },
+
+
+
+        changeNavigationState(destination) {
+
+            this.showStudentsList = false
+            this.showCreateForm = false
+            this.showHome = false
+            switch (destination) {
+                case "home":
+                    this.showHome = true
+                    break;
+                case "create":
+                    this.showCreateForm = true
+                    break;
+
+                case "list":
+                    this.showStudentsList = true
+                    break;
+
+
+                default:
+                    this.showHome = true
+
+
+
+            }
+
+        }
+
+             
+    }
+}
+
+Vue.createApp(App).mount("#app")
